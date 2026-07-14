5df07f4c4df3f325c70344a8d98d854e507d6a62
Shervin Schneiser <shervinschneiser@gmail.com>
2026-07-14 19:30:16 +0330

    chore: initialize FastAPI project

---
+++ apps/backend/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Cthulhu API Gateway",
+    version="0.1.0",
+)
+
+
+@app.get("/")
+async def root() -> dict[str, str]:
+    return {"message": "Cthulhu API Gateway"}
