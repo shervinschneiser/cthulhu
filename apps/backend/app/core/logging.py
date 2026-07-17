3dc18b822571f270b2e8cd789b4e03985670ec90
Shervin Schneiser <shervinschneiser@gmail.com>
2026-07-17 17:30:34 +0330

    refactor(logging): configure structured logging

--- apps/backend/app/core/logging.py
+++ apps/backend/app/core/logging.py
@@ -5,13 +5,22 @@ import structlog
 
 
 def configure_logging() -> None:
+    timestamper = structlog.processors.TimeStamper(fmt="iso")
+
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.add_log_level,
+            timestamper,
+            structlog.processors.JSONRenderer(),
+        ],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
     logging.basicConfig(
         format="%(message)s",
         stream=sys.stdout,
         level=logging.INFO,
-    )
-
-    structlog.configure(
-        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
-        logger_factory=structlog.PrintLoggerFactory(),
     )
