55b6ae1773d77ec64d9d367010c781c52bc70e91
Shervin Schneiser <shervinschneiser@gmail.com>
2026-07-31 14:55:07 +0330

    feat(db): add database session dependency

--- apps/backend/app/db/__init__.py
+++ apps/backend/app/db/__init__.py
@@ -1,8 +1,5 @@
 from app.db.base import Base
 from app.db.session import SessionLocal, engine
+from app.db.dependencies import get_db
 
-__all__ = (
-    "Base",
-    "SessionLocal",
-    "engine",
-)
+__all__ = ("Base", "SessionLocal", "engine", "get_db")
