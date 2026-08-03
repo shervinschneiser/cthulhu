bc5b0817a27f9c26732f1da26903d3762806e3f7
Shervin Schneiser <shervinschneiser@gmail.com>
2026-08-03 21:22:45 +0330

    refactor(proxy): extract proxy header constants

---
+++ apps/backend/app/proxy/constants.py
@@ -0,0 +1,11 @@
+HOP_BY_HOP_HEADERS = {
+    "connection",
+    "keep-alive",
+    "proxy-authenticate",
+    "proxy-authorization",
+    "te",
+    "trailer",
+    "transfer-encoding",
+    "upgrade",
+    "host",
+}
